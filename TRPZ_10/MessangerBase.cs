38b4fa7b4ada2d6bedf25243302a563c2ba84521
Illia <61758738+Smboriginal@users.noreply.github.com>
2020-06-13 17:37:20 +0300

    Add files via upload
    Lab_10 for TRPZ

---
+++ TRPZ_10/MessangerBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TRPZ_10
+{
+    public abstract class MessangerBase : IMessanger
+    {
+        /// Логин пользователя.
+        public string UserName { get; }
+
+        /// Пароль пользователя.
+        public string Password { get; }
+
+        /// Успешность подключения и авторизации.
+        public bool Connected { get; }
+
+        /// <summary>
+        /// Создание !!!экземпляра!!! мессенджера и авторизация.
+        /// </summary>
+        /// <param name="name"> Имя пользователя. </param>
+        /// <param name="password"> Пароль пользователя. </param>
+        public MessangerBase(string name, string password)
+        {
+            /// Проверка входных данных при авторизации.
+            if (!(string.IsNullOrWhiteSpace(name) && string.IsNullOrWhiteSpace(password)))
+            {
+                UserName = name;
+                Password = password;
+                Connected = Authorize();
+            }
+            else
+            {
+                Connected = false;
+            }
+        }
+
+        /// Выполнить авторизацию в мессенджере.
+        public abstract bool Authorize();
+
+        /// <summary>
+        /// Создать сообщение готовое для отправки.
+        /// Реализация фабричного метода, вернее говоря, интерфейса. Обьявлен - но не реализован.
+        /// </summary>
+        /// <param name="text"> Текст сообщения. </param>
+        /// <param name="source"> Отправитель сообщения. </param>
+        /// <param name="target"> Получатель сообщения. </param>
+        /// <returns> Сообщение для отправки. </returns>
+        public abstract IMessage CreateMessage(string text, string source, string target);
+    }
+}
---
+++ TRPZ_10/TelegramMessanger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TRPZ_10
+{
+    public class TelegramMessanger : MessangerBase
+    {
+        /// Создать экземпляр мессенджера Телеграма.
+        /// <param name="name"> Логин. </param>
+        /// <param name="password"> Пароль. </param>
+        public TelegramMessanger(string name, string password) : base(name, password)
+        {
+        }
+
+        /// Авторизация пользователя в Телеграме.
+        /// <returns> Успешность авторизации. </returns>
+        public override bool Authorize()
+        {
+            /// Здесь должен быть код для обращения к сервакам Телеграма, но он пропущен.
+            // Console.WriteLine($"System:  {UserName} entered Tg-system via password  {Password}\n");
+            Console.WriteLine($"System:{UserName} entered Tg-system successfully.\n");
+            return true;
+        }
+
+        /// Создать сообщение готовое для отправки в Телеграм.
+        /// Это реализация фактори метода для Телеграма.
+        /// <param name="text"> Текст сообщения Телеграма. </param>
+        /// <param name="source"> Отправитель сообщения. </param>
+        /// <param name="target"> Получатель сообщения. </param>
+        /// <returns> Сообщение Телеграма, готовое к отправке. </returns>
+        public override IMessage CreateMessage(string text, string source, string target)
+        {
+            /// Здесь можно добавить другие действия
+            var message = new TelegramMessage(text, source, target);
+            return message;
+        }
+    }
+}
