9a7317e13b0ce114b43022f77ca5f9291d3eb6f6
Illia <61758738+Smboriginal@users.noreply.github.com>
2020-06-17 19:07:45 +0300

    Lab 10 Factory Method

---
+++ FactoryMethod/Authentification.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /// <summary>
+    /// Создаем интерфейс для ролей.
+    /// </summary>
+    public interface IRole
+    {
+        public void Authentification();
+    }
+    public class AuthentificationSys : IRole
+    {
+        /// <summary>
+        /// Инициализация аутентификации.
+        /// </summary>
+        public void Authentification()
+        {
+            Console.WriteLine("System: You can authorize");
+        }
+    }
+
+    public class Director : IRole
+    {
+        /// <summary>
+        /// Аутентификация
+        /// </summary>
+        public void Authentification()
+        {
+            Console.WriteLine("Director");
+        }
+    }
+
+    public class Operator : IRole
+    {
+        public void Authentification()
+        {
+            Console.WriteLine("Operator");
+        }
+    }
+
+    public class Driver : IRole
+    {
+        public void Authentification()
+        {
+            Console.WriteLine("Driver");
+        }
+    }
+
+    public interface ICreator
+    {
+        public IRole FactoryMethod();
+    }
+
+    public class RoleCreator : ICreator
+    {
+        public IRole FactoryMethod()
+        {
+            return new AuthentificationSys();
+        }
+    }
+
+    public class DirectorCreator : ICreator
+    {
+        public IRole FactoryMethod()
+        {
+            return new Director();
+        }
+    }
+
+    public class OperatorCreator : ICreator
+    {
+        public IRole FactoryMethod()
+        {
+            return new Operator();
+        }
+    }
+
+    public class DriverCreator : ICreator
+    {
+        public IRole FactoryMethod()
+        {
+            return new Driver();
+        }
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryMethod
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            /// <summary>
+            /// Создание паттерна "Фабричный метод"
+            /// </summary>
+            ICreator[] creators = { new RoleCreator(), new DirectorCreator(), new OperatorCreator(), new DriverCreator() };
+            foreach (ICreator creator in creators)
+            {
+                IRole position = creator.FactoryMethod();
+                position.Authentification();
+            }
+
+            _ = Console.Read();
+        }
+    }
+}
